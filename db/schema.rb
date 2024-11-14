8cd0eca63ac7f241ff493a9fa45a7a0de195f41e
takanosatomi <ricebread778899@gmail.com>
2024-11-14 18:27:11 +0900

    create Userモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_14_090443) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_14_091516) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.bigint "company_id"
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
