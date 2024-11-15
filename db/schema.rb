05770574a6e642e7d5d4e5f929d1947d9ec28ef9
takanosatomi <ricebread778899@gmail.com>
2024-11-15 13:08:18 +0900

    create MachineStatusモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_15_022602) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_15_033923) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_15_022602) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "machine_statuses", force: :cascade do |t|
+    t.integer "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
