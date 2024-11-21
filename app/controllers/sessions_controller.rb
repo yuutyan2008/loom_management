4d3cc4d9c5c08457b03a440589309b63369e1083
Daiki <daqidiye@gmail.com>
2024-11-21 17:05:34 +0900

    ログイン機能を実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      completed_login(user)
+    else
+      flash.now[:alert] = '登録できませんでした'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    @current_user = nil
+    flash[:notice] = 'ログアウトしました'
+    redirect_to login_path
+  end
+
+  private
+
+  def completed_login(user)
+    if user.admin?
+      redirect_to admin_orders_path
+    else
+      redirect_to root_path
+    end
+  end
+end
