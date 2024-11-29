8dceb7d1079316e7edbaf2938bfc4bae3eb4d2d8
takanosatomi <ricebread778899@gmail.com>
2024-11-29 10:16:42 +0900

    fix 管理者 機屋ごとの詳細画面

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrdersController < ApplicationController
 before_action :order_params, only: [:create,  :update]
-before_action :set_order, only: [:edit, :show, :update, :destroy, :edit_work_processes, :update_work_processes]
+before_action :set_order, only: [:edit, :show, :update, :destroy]
 before_action :work_process_params, only: [:create, :update]
 # before_action :process_estimate_params, only: [:create, :update]
 before_action :admin_user
@@ -43,7 +43,6 @@ before_action :admin_user
       @order.work_processes.build(updated_dobby_workprocesses)
       # binding.irb
       @order.save
-
 
       redirect_to admin_orders_path, notice: "注文が作成されました"
 
@@ -88,31 +87,11 @@ before_action :admin_user
       Rails.logger.debug "注文が見つかりません"
       raise "注文が見つかりません"
     end
-  end
-
-  def update
-    if @order.update(order_params)
-      # 国際化（i18n）
-      # ja.yml に定義したフラッシュメッセージに翻訳
-      # binding.irb
-      flash[:notice] = "発注が更新されました"
-
-
-      redirect_to admin_order_path(@order)
-    else
-      flash.now[:alert] = @order.errors.full_messages.join(", ") # エラーメッセージを追加
-      render :edit
-    end
-  end
-
-  # 作業工程の編集
-  def edit_work_processes
-    # 必要なデータを渡す
     @work_processes = @order.work_processes.joins(:work_process_definition).order("work_process_definitions.sequence ASC")
   end
 
-  # 作業工程の更新
-  def update_work_processes
+  def update
+    @order.update(order_params)
     # strongparameterで許可されたフォームのname属性値を取得
     permitted_params = work_processes_params.to_h
     # フォームデータからIDを取得
@@ -120,7 +99,6 @@ before_action :admin_user
     # IDを指定してDBからデータ取得
     @work_processes = WorkProcess.where(id: work_process_ids)
 
-    # success = true # フラグ初期化
     @work_processes.each do |work_process|
       # フォームデータからこのレコードに対応するパラメータを取得、idを文字列に変換
       update_record = permitted_params[work_process.id.to_s]
@@ -183,7 +161,15 @@ before_action :admin_user
 
   def work_process_params
     params.require(:work_process).permit(
-      :start_date, process_estimate: [:machine_type_id])
+      :process_estimate_id,
+      :work_process_definition_id ,
+      :work_process_status_id,
+      :factory_estimated_completion_date,
+      :actual_completion_date,
+      :start_date,
+      process_estimate: [:machine_type_id],
+      machine_assignments: [:id, :machine_status_id]
+    )
   end
 
   def set_order
