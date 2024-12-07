e427d102dcb7030b955a21867a4f855f4c729db5
Daiki <daqidiye@gmail.com>
2024-12-07 15:37:47 +0900

    問屋のホーム画面を作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,7 +24,7 @@ class SessionsController < ApplicationController
 
   def completed_login(user)
     if user.admin?
-      redirect_to admin_orders_path
+      redirect_to admin_root_path
     else
       redirect_to root_path
     end
