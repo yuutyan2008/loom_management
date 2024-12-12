8c793d3d739417bc84313de65f8c1e903a3aba0b
Daiki <daqidiye@gmail.com>
2024-12-12 13:20:03 +0900

    ホーム画面に織機の稼働状況を一括で管理できるボタンを作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,12 @@ class Order < ApplicationRecord
   belongs_to :product_number
   belongs_to :color_number
   has_many :work_processes, -> { ordered }
+  has_many :machine_assignments, through: :work_processes
 
   accepts_nested_attributes_for :work_processes
+
+  # 最新の MachineAssignment を取得するメソッド
+  def latest_machine_assignment
+    machine_assignments.order(created_at: :desc).first
+  end
 end
