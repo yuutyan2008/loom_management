60b225e9f2045028c449a696f4fdb2b007e451b1
takanosatomi <ricebread778899@gmail.com>
2024-12-16 15:51:05 +0900

    fixprodurtion.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,11 +25,9 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
-  config.assets.debug = true
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
@@ -74,7 +72,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "loom_management_production"
+  # config.active_job.queue_name_prefix = "myapp_production"
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
