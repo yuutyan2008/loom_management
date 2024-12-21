443e7bca42fc1b6f85934644da6b26aa8a4a061c
takanosatomi <ricebread778899@gmail.com>
2024-12-21 19:42:54 +0900

    fix ガント受注のみ表示

--- app/controllers/admin/machines_controller.rb
+++ app/controllers/admin/machines_controller.rb
@@ -95,64 +95,49 @@ class Admin::MachinesController < ApplicationController
   end
 
   def gant_index
-    @orders = Order.includes(:work_processes, :company)
-    colors = [ "class-a", "class-b" ]
+    # 発注情報の取得
+    @orders = Order.includes(:work_processes, :company, work_processes: [:work_process_definition, :work_process_status, process_estimate: :machine_type])
 
-    # 現在の作業工程を取得
-    # current_work_process = WorkProcess.current_work_process
+    # 過去の発注に関連付けられていない機械を取得
+    @machines = Machine.not_in_past_orders
 
+    # 受注中のみ表示
+    @orders = @orders.select do |order|
+      order.work_processes.any? do |process|
+        if process.machine_assignments.empty?
+          true # 織機割り当てのない発注も表示
+        else
+          # machine_assignments が存在する場合は、assignment の machine_id が @machines の中に含まれているかを確認
+          process.machine_assignments.any? do |assignment|
+            @machines.map { |machine| machine.id }.include?(assignment.machine_id)
+          end
+        end
+      end
+    end
+
+    # JSON フォーマット用のマッピング処理
+    colors = ["class-a", "class-b"]
     @orders = @orders.map.with_index do |order, order_index|
-      # custom_classにクラスを指定して代入
       custom_class = colors[order_index % 2]
-      order.work_processes.map { |process|
-        # tmp = order.work_processes[0..order.work_processes.find_index(process)].map(&:id)
-        # tmp.delete(process.id)
+
+      order.work_processes.map do |process|
         {
           product_number: order.product_number.number,
           company: order.company.name,
-          machine: machine_names(order),
+          machine: machine_names(order), # 単一の織機を取得
           id: process.id.to_s,
           name: process.work_process_definition.name,
           work_process_status: process.work_process_status.name,
           end: process&.earliest_estimated_completion_date&.strftime("%Y-%m-%d"),
-          # end: process&.factory_estimated_completion_date&.strftime('%Y-%m-%d'),
           start: process&.start_date&.strftime("%Y-%m-%d"),
-          # actual_completion_date: process&.actual_completion_date&.strftime('%Y-%m-%d'),
           progress: 100,
-          # dependencies: tmp.join(",")
           custom_index: order.id,
           custom_class: custom_class
         }
-      }
-    end.flatten.to_json
+      end
+    end.compact.flatten.to_json
   end
 
-  def orders
-    # DBからデータを取得
-    @orders = Order.includes(:work_processes)
-
-    # ガントチャート用のフォーマットに変換
-    render json: @orders.map { |order|
-      {
-        machine_name: machine_names(order),
-        id: order.id,
-        name: order.company.name,
-        product_number: order.product_number.number,
-        machine_status: machine_statuses_for_order(order),
-
-        work_processes: order.work_processes.map { |process|
-        {
-          name: process.work_process_definition.name,
-          work_process_status: process.work_process_status.name,
-          start: process.earliest_estimated_completion_date.strftime("%Y-%m-%d"),
-          end: process.factory_estimated_completion_date.strftime("%Y-%m-%d"),
-          start_date: process.start_date.strftime("%Y-%m-%d"),
-          actual_completion_date: process.actual_completion_date.strftime("%Y-%m-%d")
-        }
-      }
-    }
-  }
-  end
 
   private
 
