0d3ff21f56b3dfa601083b29f118504285309957
Daiki <daqidiye@gmail.com>
2024-12-23 09:30:04 +0900

    flashメッセージを折りたたみ式にする

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  include FlashHelper
   before_action :require_login, only: [:index, :update]
 
   def index
@@ -59,77 +60,47 @@ class HomeController < ApplicationController
     completed_status = WorkProcessStatus.find_by(name: '作業完了')
     return unless completed_status
 
-    # 対象となるWorkProcessを取得（Orderとの関連を事前に読み込み）
     overdue_work_processes = WorkProcess.includes(:order, :work_process_definition)
                                         .where(order_id: orders.ids)
                                         .where("factory_estimated_completion_date < ?", Date.today)
                                         .where.not(work_process_status_id: completed_status.id)
 
     if overdue_work_processes.exists?
-      # Orderごとにグループ化
       grouped = overdue_work_processes.group_by(&:order)
-      # 件数の取得
       total_overdue_orders = grouped.keys.size
 
-      # メッセージリストを作成
-      message = grouped.map do |order, work_processes|
-        wp_links = work_processes.each_with_index.map do |wp, index|
-          if index == 0
-            # 最初のWorkProcessには受注IDを含める
-            "受注 ID:#{order.id} 作業工程: #{wp.work_process_definition.name}"
-          else
-            # 以降のWorkProcessは受注IDを含めずに作業工程のみ
-            "作業工程: #{wp.work_process_definition.name}"
-          end
-        end.join(", ")
-
-        # 受注IDごとにリンクを生成
-        view_context.link_to wp_links, order_path(order), class: "underline"
-      end.join("<br>").html_safe
-
-      # フラッシュメッセージにリンクを含めて設定
-      flash.now[:alert] = <<-HTML.html_safe
-        <strong>予定納期が過ぎている受注が #{total_overdue_orders} 件あります。</strong>
-        <ul class="text-red-700 list-disc ml-4 px-4 py-2">
-          <li>#{message}</li>
-        </ul>
-      HTML
+      flash.now[:alerts] ||= []
+      flash.now[:alerts] << build_flash_alert_message(
+        "予定納期が過ぎている受注が #{total_overdue_orders} 件あります。",
+        grouped.keys,
+        ->(order) { edit_order_path(order) },
+        ->(order) { order_path(order) }
+      )
     end
   end
 
   ## 追加: 織機の割り当てができていない受注をチェックするメソッド
   def check_missing_machine_assignments(orders)
-    # 現在のユーザーの会社に関連するOrderを取得
     orders = Order.where(company_id: current_user&.company&.id)
 
-    # MachineAssignmentが全く存在しないOrderを特定
     orders_without_assignment = orders.left_outer_joins(:machine_assignments)
                                       .where(machine_assignments: { machine_id: nil })
                                       .distinct
-    # 過去に受注したOrderを特定
     all_work_process_definition_ids = [1, 2, 3, 4, 5]
     orders_with_work_process = orders.joins(:work_processes)
                                      .where(work_processes: { work_process_definition_id: all_work_process_definition_ids })
                                      .where.not(work_processes: { work_process_status_id: 3 })
                                      .distinct
-    # 両方の条件を満たすOrderを取得
     matched_orders = orders_without_assignment.where(id: orders_with_work_process.select(:id))
 
     if matched_orders.any?
-      # メッセージリストを作成
-      message = matched_orders.map do |order|
-        # 受注IDごとにリンクを生成
-        message = view_context.link_to "受注 ID:#{order.id} の編集へ移動", edit_order_path(order), class: "underline"
-        "<li>#{message}</li>"
-      end.join("<br>").html_safe
-
-      # フラッシュメッセージにリンクを含めて設定
-      flash.now[:alert] = <<-HTML.html_safe
-        <strong>以下の受注は織機の割り当てができていません。</strong>
-        <ul class="text-red-700 list-disc ml-4 px-4 py-2">
-          #{message}
-        </ul>
-      HTML
+      flash.now[:alerts] ||= []
+      flash.now[:alerts] << build_flash_alert_message(
+        "以下の受注は織機の割り当てができていません。",
+        matched_orders,
+        ->(order) { edit_order_path(order) },
+        ->(order) { order_path(order) }
+      )
     end
   end
 end
