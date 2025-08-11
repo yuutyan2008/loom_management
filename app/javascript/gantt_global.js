f68a4028fa84ddd9ef5b789795e24f4270400d61
takanosatomi <ricebread778899@gmail.com>
2025-08-11 18:03:58 +0900

    gant.jsの読み込みを一旦元に戻す

--- app/javascript/gantt_global.js
+++
@@ -1,6 +0,0 @@
-import Gantt from "gantt-fix";
-
-// Expose as global to keep backward compatibility with inline scripts
-// that call `new Gantt(...)` without ESM imports.
-window.Gantt = Gantt;
-
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,6 +4,5 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "gantt-fix", to: "frappe-gantt.es.js"
 pin "orderform_no_react"
-pin "gantt_global"
 
 pin "@rails/request.js", to: "request.js", preload: true
